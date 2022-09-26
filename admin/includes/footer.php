2b8220e6183cb42395212c2f3233caf25d32dea8
camilo180601 <camilo06180401@gmail.com>
2022-09-26 01:03:12 -0500

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Todos los derechos reservados</b>
+    </div>
+   
+    <strong>Copyright &copy; <?= date("Y"); ?></strong>
+</footer>
